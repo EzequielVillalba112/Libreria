f9fddd2251a0f39cf94f963e9ba981a4bbf32885
Ezequiel Villalba <148280303+EzequielVillalba112@users.noreply.github.com>
2024-01-21 11:45:43 -0300

    1

--- src/validation/login/loginValidate.js
+++ src/validation/login/loginValidate.js
@@ -1,13 +1,20 @@
-export const validateLogin = (email,password) =>{
-    if(email != "" && password != ""){
+/**
+ * Validates a login by checking if the email and password are valid.
+ *
+ * @param {string} email - The email address to validate.
+ * @param {string} password - The password to validate.
+ * @returns {boolean|string} - Returns true if the email and password are valid. Otherwise, returns an error message.
+ */
+export const validateLogin = (email, password) => {
+    if (email !== "" && password !== "") {
         let emailExpReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if(emailExpReg.test(email)){
+        if (emailExpReg.test(email)) {
             return true;
-        }else{
+        } else {
             const message = "Correo inválido.";
             return message;
         }
-    }else{
+    } else {
         const message = "Campos vacíos.";
         return message;
     }
